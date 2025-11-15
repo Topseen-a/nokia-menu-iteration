3dc50bf252330e00c61c8770c28c62be359a6854
Topseen-a <abodunrintopseen@gmail.com>
2025-11-15 15:01:13 +0100

    fourth commit

--- first-iteration/NokiaMenuMapOne.java
+++ first-iteration/NokiaMenuMapOne.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-    public class Nokia{
+    public class NokiaMenuMapOne{
         public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
